b49bbf0c76539fd9bdaba668728162519b43cbc1
Kushagra <oyekushagra22@users.noreply.github.com>
2026-03-07 13:11:06 +0530

    code ion c++

--- Dynamic_programing/climbing_stairs.cpp
+++ Dynamic_programing/climbing_stairs.cpp
@@ -18,3 +18,30 @@ Explanation: There are three ways to climb to the top.
 3. 2 steps + 1 step
 
 Constraints: 1 <= n <= 45*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int climbStairs(int n) {
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+
+    vector<int> dp(n + 1);
+    dp[1] = 1; // 1 way to climb 1 step
+    dp[2] = 2; // 2 ways to climb 2 steps
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2]; // The number of ways to climb to the current step
+    }
+
+    return dp[n];
+}
+
+int main() {
+    int n;
+    cin>> n;
+    cout << "Number of ways to climb " << n << " steps: " << climbStairs(n) <<endl;
+    return 0;
+}
